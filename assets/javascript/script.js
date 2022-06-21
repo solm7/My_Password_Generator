668dcdac60406b01436e154c24fb6d1b909441a3
Roronoa <dup1040@gmail.com>
2022-06-20 22:01:20 -0400

    Second Commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = prompt("How long do you want your password?");
+  console.log(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Entry Invalid! Please try again.");
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
