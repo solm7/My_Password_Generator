c396de6899375d36eced7c35025a721d4559c6a3
Roronoa <dup1040@gmail.com>
2022-06-20 22:59:56 -0400

    Third Commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,10 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var passwordLength = prompt("How long do you want your password?");
-  console.log(passwordLength);
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert("Entry Invalid! Please try again.");
+  } else {
+    var lowerCase = confirm("Do you want Lower Case?");
+    var upperCase = confirm("Do you want Upper Case?");
+    var number = confirm("Do you want Numbers?");
+    var specialChar = confirm("Do you want Special Characters?");
   }
+  function pickLetter(shouldUpperCase) {
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    var myLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    if (shouldUpperCase) {
+      return myLetter.toUpperCase();
+    } else {
+      return myLetter;
+    }
+
+
+  }
+  console.log(pickLetter());
+  console.log(pickLetter(true));
 }
 
 // Write password to the #password input
